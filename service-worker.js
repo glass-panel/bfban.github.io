2b0ac84407fef6c5e07d92e1670f04cf85c65371
glass-panel <63582311+glass-panel@users.noreply.github.com>
2023-10-12 12:00:54 +0000

    build front

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-importScripts("/precache-manifest.ef8d4c232cdbd8cfd530d7e1053ca63a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-self.addEventListener('fetch', function (event) {
-  console.log(event);
-});
