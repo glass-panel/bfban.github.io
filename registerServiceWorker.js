6f66e28ba787147f3c05b2e8bfb10318155c1645
glass-panel <63582311+glass-panel@users.noreply.github.com>
2024-09-11 15:14:17 +0000

    build front

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.86a726fd57d62703dccbd11b9dbd27d6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.18f0820ce1a105a76ee0be6d1d5b5738.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 import { register } from 'register-service-worker'
